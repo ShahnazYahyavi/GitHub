795ef0775c209be7a1e24a6f8555a7709c5d456d
ShahnazYahyavi <33924824+ShahnazYahyavi@users.noreply.github.com>
2017-12-02 14:01:40 +0100

    AssertEquals med inkomest og get inkomestskat fra klassen skat

---
+++ Skat/src/tast/Skat.java
@@ -0,0 +1,18 @@
+package tast;
+
+
+
+public class Skat {
+private double inkomest;
+private double fradrage;
+
+
+public double getInkomestSkat() {
+return inkomest-(inkomest-inkomest)*(26.6/100);
+
+}
+
+
+
+
+}
--- Skat/src/tast/skatTest.java
+++ Skat/src/tast/skatTest.java
@@ -2,7 +2,7 @@ package tast;
 
 import static org.junit.Assert.*;
 
-import java.math.BigDecimal;
+
 
 import org.junit.Test;
 
@@ -10,12 +10,11 @@ public class skatTest {
 
 	@Test
 	public void testBegeneInkomestSkat() {
-		
-		double løn=400000;
-		double fraDrag;
-		double fradrag= løn-46000;
-		double inkomestSkat= løn-(løn-fradrag)*(26.6/100);
-		assertEquals(inkomestSkat,skat.getInkomestSkat());
+		Skat skat= new Skat();
+		double inkomest=400000;
+		double fradrag= inkomest-46000;
+		double inkomestSkat= inkomest-(inkomest-fradrag)*(26.6/100);
+		assertEquals(inkomestSkat,skat.getInkomestSkat(),1);
 	}
 
 }
