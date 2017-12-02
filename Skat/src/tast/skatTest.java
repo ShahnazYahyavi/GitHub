3b0d3a27742481dadd701b322e34dedd73a0734c
ShahnazYahyavi <33924824+ShahnazYahyavi@users.noreply.github.com>
2017-12-02 17:29:00 +0100

    BegeneBundSkatTilLønmodtagerMedÆgtefælle

--- Skat/src/tast/skatTest.java
+++ Skat/src/tast/skatTest.java
@@ -2,50 +2,57 @@ package tast;
 
 import static org.junit.Assert.*;
 
-
-
 import org.junit.Test;
 
 public class skatTest {
 
 	@Test
 	public void testBegeneInkomestSkat() {
-		Lønmodtager skat= new Lønmodtager(400000, 46000);
-		
-		double inkomestSkat= 400000-(400000-46000)*(26.6/100);
-		assertEquals(inkomestSkat,skat.getInkomestSkat(),1);
+		Lønmodtager skat = new Lønmodtager(400000, 46000);
+
+		double inkomestSkat = 400000 - (400000 - 46000) * (26.6 / 100);
+		assertEquals(inkomestSkat, skat.getInkomestSkat(), 1);
 	}
+
 	@Test
 	public void testBegeneBundSkat() {
-		Lønmodtager skat= new Lønmodtager(400000, 46000);
-		
-		double bundSkat= 400000-(400000-46000)*(11.15/100);
-		assertEquals(bundSkat,skat.getBundSkatSkat(),1);
+		Lønmodtager skat = new Lønmodtager(400000, 46000);
+
+		double bundSkat = 400000 - (400000 - 46000) * (11.15 / 100);
+		assertEquals(bundSkat, skat.getBundSkatSkat(), 1);
 	}
+
 	@Test
 	public void testBegeneTopSkat() {
-		Lønmodtager skat= new Lønmodtager(400000, 46000);
-	double inkomest=400000;
-	double topSkat= 400000-498900;
-		
-		if(topSkat>0 && inkomest>43800) {
-			topSkat= 400000-(400000-498900)*(14.27/100);
-			double a= 400000-(400000-498900)*(4.25/100);
-			topSkat=topSkat+a;
-		}
-		else 
-			topSkat=0;
-		
-			
-		assertEquals(topSkat,skat.getTopSkatSkat(),1);
+		Lønmodtager skat = new Lønmodtager(400000, 46000);
+		double inkomest = 400000;
+		double topSkat = 400000 - 498900;
+
+		if (topSkat > 0 && inkomest > 43800) {
+			topSkat = 400000 - (400000 - 498900) * (14.27 / 100);
+			double a = 400000 - (400000 - 498900) * (4.25 / 100);
+			topSkat = topSkat + a;
+		} else
+			topSkat = 0;
+
+		assertEquals(topSkat, skat.getTopSkatSkat(), 1);
 	}
+
 	@Test
 	public void testBegeneInkomestSkatMedægtefælle() {
-		
-		Lønmodtager skat1= new Lønmodtager(400000,400000,46000,34500,true); // den lønmotager er gift
-				
-		double inkomestSkat=( 400000-(400000-46000)*(26.6/100))+(400000-(400000-34500)*(26.6/100));
-		
-		assertEquals(inkomestSkat,skat1.getInkomestSkat(),1);
+
+		Lønmodtager skat = new Lønmodtager(400000, 400000, 46000, 34500, true); // den lønmotager er gift
+
+		double inkomestSkat = (400000 - (400000 - 46000) * (26.6 / 100)) + (400000 - (400000 - 34500) * (26.6 / 100));
+
+		assertEquals(inkomestSkat, skat.getInkomestSkat(), 1);
 	}
+	@Test
+	public void testBegeneBundSkatTilLønmodtagerMedÆgtefælle() {
+		Lønmodtager skat = new Lønmodtager(400000, 400000, 46000, 34500, true);
+
+		double bundSkat = 400000 - (400000 - 46000) * (11.15 / 100)+(400000 - (400000 - 34500) * (11.15 / 100));
+		assertEquals(bundSkat, skat.getBundSkatSkat(), 1);
+	}
+	
 }
